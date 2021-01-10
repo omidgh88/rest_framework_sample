29ee042be26b436b170ff20519bcb727b2a82db7
omidgholami88 <omidgholami88@gmail.com>
2021-01-10 16:27:59 +0330

    add __str__ to rental models

--- app/rental/models.py
+++ app/rental/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Friend(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Belonging(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Borrowed(models.Model):
@@ -14,3 +20,6 @@ class Borrowed(models.Model):
     to_who = models.ForeignKey(Friend, on_delete=models.CASCADE)
     when = models.DateField(auto_now=True)
     returned = models.DateField(blank=True,  null=True)
+
+    def __str__(self):
+        return f"{self.what} to {self.to_who} at {self.when}"
